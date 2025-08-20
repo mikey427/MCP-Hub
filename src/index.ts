7df75fb6150eea2f8b236e2f068431bc9e4e6803
Michael <mrbaez427@gmail.com>
2025-08-20 00:53:30 -0700

    Restructured project

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import fs from "fs";
+// const fs = require("fs").promises;
+
+async function main() {
+  console.log("hello from MCP Hub!");
+
+  const rawServerData = await fs.readFileSync("./servers.JSON", "utf8");
+  const mcpServers = JSON.parse(rawServerData);
+  console.log(mcpServers);
+}
+
+main();
